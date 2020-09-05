cf6e08fed6c739f5d4de17f462aff89871da1146
crystalfn <969274162@qq.com>
2020-09-05 17:37:53 +0800

    [fangni] feat: complete exercise_01 - numberFormatter

--- src/exercise_01/numberFormatter.js
+++ src/exercise_01/numberFormatter.js
@@ -8,5 +8,12 @@ export default function formatNumber(number, option) {
   // * Please implement the function and pass all the tests in format_number_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (typeof number !== 'number'
+    || (option && Object.prototype.toString.call(option) !== '[object Object]')) {
+    return new Error('invalid input');
+  }
+
+  const result = number.toFixed(2).toString();
+  if (option && option.currency) { return `$ ${result}`; }
+  return result;
 }
