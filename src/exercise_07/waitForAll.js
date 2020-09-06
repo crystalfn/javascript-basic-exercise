924f7cbbbf3972458713faf7fdbefa70f2941969
crystalfn <969274162@qq.com>
2020-09-06 23:10:31 +0800

    [fangni] feat: implement exercise_07 - waitForAll

--- src/exercise_07/waitForAll.js
+++ src/exercise_07/waitForAll.js
@@ -9,5 +9,34 @@ export default function waitForAll(...promises) {
   // * Please implement this function and pass all the tests in wait_for_all_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  let isAllElementArePromise = false;
+  isAllElementArePromise = promises.every(item => Object.prototype.toString.call(item) === '[object Promise]');
+  if (!isAllElementArePromise) {
+    throw new Error('Not all elements are promises.');
+  }
+
+  let promisesCompleted = 0;
+  let isError = false;
+  return new Promise((resolve, reject) => {
+    promises.forEach((promise) => {
+      promise.then(() => {
+        promisesCompleted += 1;
+        if (promisesCompleted === promises.length) {
+          if (isError) {
+            reject();
+          } else {
+            resolve();
+          }
+        }
+      })
+        .catch(() => {
+          promisesCompleted += 1;
+          if (promisesCompleted === promises.length) {
+            reject();
+          } else {
+            isError = true;
+          }
+        });
+    });
+  });
 }
