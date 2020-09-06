8fe433066423ab9f223f2a51234e986c90e12151
crystalfn <969274162@qq.com>
2020-09-06 15:57:15 +0800

    [fangni] feat: implement exercise_04 - arrayToList

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,17 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (!array) {
+    throw new Error('Creating list from undefined array');
+  }
+  if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  }
+
+  let list = null;
+  for (let i = array.length - 1; i >= 0; i -= 1) {
+    list = { value: array[i], next: list };
+  }
+
+  return list;
 }
