087b3a700148b0b16d8ea4c630315adcf9699588
crystalfn <969274162@qq.com>
2020-09-06 17:40:40 +0800

    [fangni] feat: implement exercise_06 - vector

--- src/exercise_06/vector.js
+++ src/exercise_06/vector.js
@@ -12,5 +12,20 @@ export default class Vector {
   // * Please do NOT modify the signature of the class. Please put all your code in the Vector
   // class.
 
-  // PLEASE DELETE THIS LINE AND ADD YOUR IMPLEMENTATION HERE
+  constructor(x, y) {
+    Object.defineProperty(this, 'x', { value: x, writable: false });
+    Object.defineProperty(this, 'y', { value: y, writable: false });
+  }
+
+  static plus(vector1, vector2) {
+    return new Vector(vector1.x + vector2.x, vector1.y + vector2.y);
+  }
+
+  static minus(vector1, vector2) {
+    return new Vector(vector1.x - vector2.x, vector1.y - vector2.y);
+  }
+
+  distance() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
 }
